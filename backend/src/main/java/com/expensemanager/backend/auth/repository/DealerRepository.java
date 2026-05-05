9c44775c8e7b24ab1b6d0e6b30f0419e3a2a8526
MarcusFranklin <marcus.franklin.j12@gmail.com>
2026-05-06 00:44:57 +0530

    test push

--- backend/src/main/java/com/expensemanager/backend/auth/repository/DealerRepository.java
+++ backend/src/main/java/com/expensemanager/backend/auth/repository/DealerRepository.java
@@ -10,4 +10,5 @@ public interface DealerRepository extends MongoRepository<Dealer , String>{
     Optional<Dealer> findByPhine(String phone);
 }
 
-// The DealerRepository interface extends MongoRepository, providing CRUD operations for the Dealer entity. It also includes a custom method findByPhone to retrieve a Dealer by their phone number.
+// The DealerRepository interface extends MongoRepository, providing CRUD operations for the Dealer entity. 
+//It also includes a custom method findByPhone to retrieve a Dealer by their phone number.
